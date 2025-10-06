805eaed84ecf545ed003d47c517b7a4c8e0956c6
Bastien BUIL <bastien.buil@orange.com>
2025-10-06 19:09:07 +0200

    Refactor elf-reader script
    Signed-off-by: Bastien BUIL <bastien.buil@orange.com>

--- src/elf-reader/reader.py
+++ src/elf-reader/reader.py
@@ -1,23 +1,28 @@
 """
- Software Name: CS4WAMR
- SPDX-FileCopyrightText: Copyright (c) Orange SA
- SPDX-License-Identifier: MIT
- 
- This software is distributed under the MIT licence,
- see the "LICENSE" file for more details or https://opensource.org/license/mit
+Software Name: CS4WAMR
+SPDX-FileCopyrightText: Copyright (c) Orange SA
+SPDX-License-Identifier: MIT
+
+This software is distributed under the MIT licence,
+see the "LICENSE" file for more details or https://opensource.org/license/mit
 """
 
 from elf_utils import parse_elf, parse_elf_section, find_symbol_position
 from binary_utils import read_at_pos, write_at_pos
 import argparse
 
+
 def partition_lib_symbols(lib_path, symbols, filter_fnc=lambda x: False):
     lib_symbols = []
     other_symbols = []
 
     for symbol in symbols:
-        if symbol["path"].startswith(lib_path) \
-                and symbol["type"] != "t" and symbol["type"] != "T" and not filter_fnc(symbol):
+        if (
+            symbol["path"].startswith(lib_path)
+            and symbol["type"] != "t"
+            and symbol["type"] != "T"
+            and not filter_fnc(symbol)
+        ):
             lib_symbols.append(symbol)
         else:
             other_symbols.append(symbol)
@@ -31,41 +36,62 @@ def insert_in_group(group, symbol):
         group["max_address"] = symbol["address"] + symbol["size"]
     group["symbols"].append(symbol)
 
+
 def create_first_symbol_group(symbols):
     """
-        max_address is not included !!!!!
+    max_address is not included !!!!!
     """
-    group = {"min_address": symbols[0]["address"], "max_address": symbols[0]["address"] + symbols[0]["size"], "symbols": [symbols[0]]}
+    group = {
+        "min_address": symbols[0]["address"],
+        "max_address": symbols[0]["address"] + symbols[0]["size"],
+        "symbols": [symbols[0]],
+    }
     for i in range(1, len(symbols)):
         symbol = symbols[i]
         insert_in_group(group, symbol)
     return group
+
 
 def split_groups_with_other_symbols(groups, other_symbols):
     for other_symbol in other_symbols:
         i = 0
         while i < len(groups):
             group = groups[i]
-            if (group["min_address"] < other_symbol["address"] < group["max_address"]) or \
-                (group["min_address"] < other_symbol["address"] + other_symbol["size"] < group["max_address"]):
-                new_group = {"min_address": group["max_address"], "max_address": group["min_address"], "symbols": []}
+            if (
+                group["min_address"] < other_symbol["address"] < group["max_address"]
+            ) or (
+                group["min_address"]
+                < other_symbol["address"] + other_symbol["size"]
+                < group["max_address"]
+            ):
+                new_group = {
+                    "min_address": group["max_address"],
+                    "max_address": group["min_address"],
+                    "symbols": [],
+                }
                 j = 0
                 while j < len(group["symbols"]):
                     group_symbol = group["symbols"][j]
-                    if other_symbol["address"] >= group_symbol["address"] + group_symbol["size"]:
+                    if (
+                        other_symbol["address"]
+                        >= group_symbol["address"] + group_symbol["size"]
+                    ):
                         j += 1
                     else:
                         insert_in_group(new_group, group_symbol)
                         group["symbols"].remove(group_symbol)
                 if len(new_group["symbols"]) > 0:
                     groups.append(new_group)
-                group["max_address"] = max([symbol["address"] + symbol["size"] for symbol in group["symbols"]])
+                group["max_address"] = max(
+                    [symbol["address"] + symbol["size"] for symbol in group["symbols"]]
+                )
             i += 1
+
 
 def write_group(elf_file, offset, group):
     group_size = group["max_address"] - group["min_address"]
     write_at_pos(elf_file, offset, group["min_address"])
-    write_at_pos(elf_file, offset+4, group_size)
+    write_at_pos(elf_file, offset + 4, group_size)
     return 8
 
 
@@ -80,8 +106,10 @@ def get_ignored_symbols(ignored_symbols_files, ignored_symbols_type_files):
             ignored_symbols_type = ignored_symbols_type + f.read().splitlines()
     return ignored_symbols, ignored_symbols_type
 
+
 def filter_symbols_with_list(symbols, ignored_symbols):
     return list(filter(lambda x: x["symbol"] not in ignored_symbols, symbols))
+
 
 def filter_symbols_by_type(symbols, ignored_types):
     return list(filter(lambda x: x["type"] not in ignored_types, symbols))
@@ -109,21 +137,29 @@ def main(
         print("sections:", sections)
     print("===================")
     symbols = parse_elf(elf_file)
-   
-    filter_function = lambda x: x["type"] in ignored_symbols_types or x["symbol"] in ignored_symbols
-    lib_symbols, other_symbols = partition_lib_symbols(wamr_path, symbols, filter_function)
+
+    filter_function = (
+        lambda x: x["type"] in ignored_symbols_types or x["symbol"] in ignored_symbols
+    )
+    lib_symbols, other_symbols = partition_lib_symbols(
+        wamr_path, symbols, filter_function
+    )
 
     if len(lib_symbols) == 0:
         print("Error: No lib symbol matched.")
         return
-    
+
     groups = [create_first_symbol_group(lib_symbols)]
 
     split_groups_with_other_symbols(groups, other_symbols)
-        
-    external_env_var_count_offset = find_symbol_position(sections, other_symbols, external_env_count_var_name)
+
+    external_env_var_count_offset = find_symbol_position(
+        sections, other_symbols, external_env_count_var_name
+    )
     external_env_var_count = read_at_pos(elf_file, external_env_var_count_offset)
-    external_env_var_offset = find_symbol_position(sections, other_symbols, external_env_var_name)
+    external_env_var_offset = find_symbol_position(
+        sections, other_symbols, external_env_var_name
+    )
     print(
         "Maximum number of environments:", external_env_var_count
     )  # , external_env_var_count_offset
@@ -133,7 +169,7 @@ def main(
         print("Not enought slot for external env")
         print("len group: ", len(groups))
         return
-    
+
     address_offset = external_env_var_offset
 
     for group in groups:
@@ -143,7 +179,7 @@ def main(
         print(f"group address: {group['min_address']} = 0x{hex(group['min_address'])}")
         print("group size:", group["max_address"] - group["min_address"])
         print("\n====================")
-   
+
     write_at_pos(elf_file, external_env_var_count_offset, len(groups))
     print("number of group: ", len(groups))
     print(
@@ -158,7 +194,7 @@ def main(
         sum([group["max_address"] - group["min_address"] for group in groups]),
     )
 
-    
+
 if __name__ == "__main__":
     p = argparse.ArgumentParser(
         description="A script to inject WAMR static variables address in variables of the given firmware. This script allows static_context_switcher to work. It works by grouping static variable in group and writing the group size and address to the firmware."
